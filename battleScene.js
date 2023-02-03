a36b6a72f1c722273dbf532e0396aa3db785b4cd
Leonardo Gama <leogama09@gmail.com>
2023-02-03 12:22:09 -0300

    Battle end

--- battleScene.js
+++ battleScene.js
@@ -43,6 +43,13 @@ const battleBackgroundImage = new Image()
                 renderedSprites
             })
 
+            if (draggle.health <= 0) {
+                queue.push(() => {
+                    draggle.faint()
+                })
+            }
+
+            // draggle or enemy attacks right here
             const randomAttack = 
                 draggle.attacks[Math.floor(Math.random() * draggle.attacks.length)]
 
@@ -52,6 +59,12 @@ const battleBackgroundImage = new Image()
                     recipient: emby,
                     renderedSprites
                 })
+
+                if (emby.health <= 0) {
+                    queue.push(() => {
+                        emby.faint()
+                    })
+                }
             })
         })
 
--- classes.js
+++ classes.js
@@ -84,9 +84,20 @@ class Monster extends Sprite {
         this.attacks = attacks
     }
 
+    faint() {
+        document.querySelector('#dialogueBox').innerHTML = this.name + ' fainted!'
+        gsap.to(this.position, {
+            y: this.position.y + 20
+        })
+        gsap.to(this, {
+            opacity: 0
+        })
+    }
+
     attack({attack, recipient, renderedSprites}) {
         document.querySelector('#dialogueBox').style.display = 'block'
-        document.querySelector('#dialogueBox').innerHTML = this.name + ' used ' + attack.name
+        document.querySelector('#dialogueBox').innerHTML = 
+        this.name + ' used ' + attack.name
 
         let healthBar = '#enemyHealthBar'
         if (this.isEnemy) healthBar = '#playerHealthBar'
@@ -94,7 +105,7 @@ class Monster extends Sprite {
         let rotation = 1
         if (this.isEnemy) rotation = -2.3
 
-        this.health -= attack.damage
+        recipient.health -= attack.damage
 
         switch (attack.name) {
             case 'Fireball':
@@ -122,7 +133,7 @@ class Monster extends Sprite {
                     onComplete: () => {
                         // Enemy actually gets hit
                         gsap.to(healthBar, {
-                            width: this.health + '%'
+                            width: recipient.health + '%'
                         })
         
                         gsap.to(recipient.position, {
@@ -157,7 +168,7 @@ class Monster extends Sprite {
                     onComplete: () => {
                         // Enemy actually gets hit
                         gsap.to(healthBar, {
-                            width: this.health + '%'
+                            width: recipient.health + '%'
                         })
         
                         gsap.to(recipient.position, {
